f16473cd974b3653cc8dac481a0596b0036b8a49
Sujata Dev <sujatadev97@gmail.com>
2017-12-23 17:50:50 +0530

    nullible type

--- src/Nullible.cs
+++ src/Nullible.cs
@@ -10,7 +10,8 @@ namespace N
             int ?b = 45;
             double ?c = new double?();
             int ?d = a ?? b;
-            Console.WriteLine(a+" "+b+" "+c+" "+d);
+            int e = a ?? -1;
+            Console.WriteLine(a+" "+b+" "+c+" "+d+" "+e);
         }
     }
 }
