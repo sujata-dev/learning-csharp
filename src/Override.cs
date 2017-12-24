773055692cc2124a777149e2b79586be97a76026
Sujata Dev <sujatadev97@gmail.com>
2017-12-24 22:32:02 +0530

    function overriding

---
+++ src/Override.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace N
+{
+    abstract class Shape
+    {
+        public abstract double area();
+    }
+
+    class Circle: Shape
+    {
+        private const double pi = 3.14;
+        private int rad;
+        public Circle(int rad)
+        {
+            this.rad = rad;
+        }
+        public override double area()
+        {
+            return pi * Math.Pow(rad, 2);
+        }
+    }
+    class Override
+    {
+        static void Main(string[] arg)
+        {
+            Circle c = new Circle(4);
+            Console.WriteLine(c.area());
+        }
+    }
+}
